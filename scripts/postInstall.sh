ccbea280c04a5868acdfc4ad7cdc8fff6f5a215a
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-02-11 14:35:00 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -7,7 +7,7 @@ sleep 120s;
 target=$(docker-compose port nginx 80)
 
 
-echo "y" |docker-compose exec -T mautic_web bash -c "php /var/www/html/bin/console mautic:install https://${target} --db_driver='pdo_mysql' --db_host='${MYSQL_HOST}' --db_port='${MYSQL_PORT}' --db_name='${MYSQL_DATABASE}' --db_user='${MYSQL_USER}' --db_password='${MYSQL_PASSWORD}' --db_backup_tables='false' --admin_email='${ADMIN_EMAIL}' --admin_password='${ADMIN_PASSWORD}' --admin_firstname='admin' --admin_lastname='admin' --admin_username='admin'"
+echo "y" |docker-compose exec -T mautic_web bash -c "php /var/www/html/bin/console mautic:install https://${CI_CD_DOMAIN} --db_driver='pdo_mysql' --db_host='${MYSQL_HOST}' --db_port='${MYSQL_PORT}' --db_name='${MYSQL_DATABASE}' --db_user='${MYSQL_USER}' --db_password='${MYSQL_PASSWORD}' --db_backup_tables='false' --admin_email='${ADMIN_EMAIL}' --admin_password='${ADMIN_PASSWORD}' --admin_firstname='admin' --admin_lastname='admin' --admin_username='admin'"
 
 
 docker-compose down;
