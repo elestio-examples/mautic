733f997bfee8a51232dc0d4029400f52cd2bdc6f
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-02-11 12:01:56 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -9,15 +9,6 @@ chown -R 1000:1000 ./mautic/config
 cat << EOT >> ./mautic/config/local.php
 <?php
 \$parameters = array(
-	'db_driver' => 'pdo_mysql',
-	'db_host' => '${MAUTIC_DB_HOST}',
-	'db_port' => '${MAUTIC_DB_PORT}',
-	'db_name' => '${MAUTIC_DB_DATABASE}',
-	'db_user' => '${MAUTIC_DB_USER}',
-	'db_password' => '${MAUTIC_DB_PASSWORD}',
-	'db_table_prefix' => null,
-	'db_backup_tables' => 1,
-	'db_backup_prefix' => 'bak_',
 	'mailer_from_name' => 'Mautic',
 	'mailer_from_email' => '${SMTP_FROM}',
 	'mailer_reply_to_email' => null,
