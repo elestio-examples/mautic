a3287dcd2299ee84da35f8e2e8f665fc64cba570
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-02-11 13:36:28 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,4 +5,6 @@ echo "Waiting for software to be ready..."
 sleep 120s;
 
 
+docker-compose exec -T mautic_worker bash -c "php /var/www/html/bin/console mautic:install https://${CI_CD_DOMAIN} --db_driver='pdo_mysql' --db_host='${MYSQL_HOST}' --db_port='${MYSQL_PORT}' --db_name='${MYSQL_DATABASE}' --db_user='${MYSQL_USER}' --db_password='${MYSQL_PASSWORD}' --db_backup_tables='false' --admin_email='${ADMIN_EMAIL}' --admin_password='${ADMIN_PASSWORD}' --admin_firstname='admin' --admin_lastname='admin' --admin_username='admin'"
+
 docker-compose exec -T mautic_web bash -c "php /var/www/html/bin/console mautic:install https://${CI_CD_DOMAIN} --db_driver='pdo_mysql' --db_host='${MYSQL_HOST}' --db_port='${MYSQL_PORT}' --db_name='${MYSQL_DATABASE}' --db_user='${MYSQL_USER}' --db_password='${MYSQL_PASSWORD}' --db_backup_tables='false' --admin_email='${ADMIN_EMAIL}' --admin_password='${ADMIN_PASSWORD}' --admin_firstname='admin' --admin_lastname='admin' --admin_username='admin'"
