98182837f35d9c48f7703a6dcfa36bb0f1b02832
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2024-03-01 15:43:38 +0530

    Update postInstall.sh

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,7 +5,7 @@ echo "Waiting for software to be ready..."
 sleep 120s;
 
 
-while ! curl -f http://172.17.0.1:9001; do sleep 5; done
+while ! curl -f http://172.17.0.1:7015; do sleep 5; done
 
 sed -i \
  -e "s\.*mailer_host.*\'mailer_host' => '172.17.0.1',\g" \
