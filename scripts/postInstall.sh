d641d68ef39d378a73a2bf6d6d2db5c3437ca804
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2024-03-01 16:40:00 +0530

    Update postInstall.sh

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -11,3 +11,6 @@ sed -i \
  -e "s\.*mailer_host.*\'mailer_host' => '172.17.0.1',\g" \
  -e "s\.*mailer_port.*\'mailer_port' => '25',\g" \
  ./mautic/app/config/local.php
+
+
+docker-compose exec -T mautic bash -c "php /var/www/html/bin/console mautic:install https://${DOMAIN} --db_driver='pdo_mysql' --db_host='mysql' --db_port='3306' --db_name='${DB_MYSQL_NAME}' --db_user='${DB_MYSQL_USER}' --db_password='${DB_MYSQL_PASSWORD}' --db_backup_tables='false' --admin_email='${EMAIL}' --admin_password='${APP_PASSWORD}' --admin_firstname='admin' --admin_lastname='admin' --admin_username='admin'"
