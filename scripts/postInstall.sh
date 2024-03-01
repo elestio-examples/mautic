256ff4942ab9a310423fe6f06e30bccd0fa9be00
amitshuklabag <amitshuklabag@gmail.com>
2024-03-01 15:25:32 +0530

    changed to v4

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,18 +5,9 @@ echo "Waiting for software to be ready..."
 sleep 120s;
 
 
+while ! curl -f http://172.17.0.1:9001; do sleep 5; done
 
-docker-compose exec -T mautic_web bash -c "php /var/www/html/bin/console mautic:install https://${CI_CD_DOMAIN} --db_driver='pdo_mysql' --db_host='${MYSQL_HOST}' --db_port='${MYSQL_PORT}' --db_name='${MYSQL_DATABASE}' --db_user='${MYSQL_USER}' --db_password='${MYSQL_PASSWORD}' --db_backup_tables='false' --admin_email='${ADMIN_EMAIL}' --admin_password='${ADMIN_PASSWORD}' --admin_firstname='admin' --admin_lastname='admin' --admin_username='admin'"
-
-
-docker-compose down;
-
-
-sed -i "s~# - MAUTIC_MESSENGER_DSN_EMAIL=doctrine://default~- MAUTIC_MESSENGER_DSN_EMAIL=doctrine://default~g" ./docker-compose.yml
-sed -i "s~# - MAUTIC_MESSENGER_DSN_HIT=doctrine://default~- MAUTIC_MESSENGER_DSN_HIT=doctrine://default~g" ./docker-compose.yml
-
-insert_line='$_SERVER['\''HTTPS'\''] = '\''on'\'';'
-sed -i "1 s|\(<?php\)|\1\n$insert_line|" "./mautic/config/local.php"
-
-
-docker-compose up -d
+sed -i \
+ -e "s\.*mailer_host.*\'mailer_host' => '172.17.0.1',\g" \
+ -e "s\.*mailer_port.*\'mailer_port' => '25',\g" \
+ ./mautic/app/config/local.php
